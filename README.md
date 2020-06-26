36e58e8b928fb2b5e1a2d16043b8d0f1ab85bce2
Shiba-Programming <shibamaruart@gmail.com>
2020-06-27 06:35:43 +0900

    js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JavaScript-Tutorial-01
+
+JavaScriptのTutorial
---
+++ Tutorial 01/Tutorial-01.js
@@ -0,0 +1,6 @@
+window.alert("document（ドキュメント）：文書。ここではHTMLのことを指している");
+window.alert("write（ライト）：書く");
+
+document.write("1足す2は");
+document.write(1+2);
+document.write("です");
