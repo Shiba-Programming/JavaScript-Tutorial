d55a7fcb87961bdeabdc73c49835bfcd2ae4a5e2
Shiba-Programming <shibamaruart@gmail.com>
2020-06-28 12:58:17 +0900

    js

---
+++ Tutorial 06/Tutorial-06.js
@@ -0,0 +1,21 @@
+window.alert("alert(警告)のalertの部分");
+
+window.alert("console.log(ログ)のlogの部分");
+
+window.alert("document.write(HTML)のwriteの部分");
+
+window.alert("どれも関数を利用する記述");
+
+window.alert("これらの関数を自分で作ることもできる");
+
+window.alert("【function logDate() {console.log(new Date()); } 】");
+
+window.alert("function(ファンクション）関数。");
+
+window.alert("Date（デート）日付。");
+
+function logDate() {
+    console.log(new Date());
+}
+logDate();
+logDate();
